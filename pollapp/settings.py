e90fcb89b791561d9032533046928ecd5b01cb6c
Swapnil <swapnilshah102002@gmail.com>
2023-01-16 16:31:33 +0530

    no secret key now gg

--- pollapp/settings.py
+++ pollapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-un(8+@7ut)&3!=4eex)u1ttuv5h+i&0w754nghhhrqmop!+75h'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
